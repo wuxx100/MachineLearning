529deec13cad9bb8c5d60dcce689e25c5895649c
WU Xuexin <xxwu100@163.com>
2017-04-19 17:17:21 +0200

    add LinerRegression standard

--- Regression/LinearRegression.py
+++ Regression/LinearRegression.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 if __name__ == '__main__':
@@ -12,8 +14,8 @@ if __name__ == '__main__':
     data = pd.read_csv('Advertising.csv')
     x=data[['TV','Radio','Newspaper']]
     y=data['Sales']
-    print x.shape
-    print y.shape
+    # print x.shape
+    # print y.shape
 
     # 取训练集与测试集
     x_train,x_test,y_train,y_test = train_test_split(x,y,train_size=0.8, random_state=1)
@@ -24,5 +26,32 @@ if __name__ == '__main__':
     print LRmodel.coef_, LRmodel.intercept_ # 参数与截距
 
     # 用模型验证测试集
+    print 'y_test=',y_test,y_test.shape
+    order = y_test.argsort()                # 将测试数据排序，从小到大，为了使数据美观
+    y_test=y_test.values[order]             # 将y从Series转换到array
+    x_test=x_test.values[order,:]
+    y_hat = LRmodel.predict(x_test)         # 预测
+    mse = np.average((y_test-y_hat)**2)
+    rmse= np.sqrt(mse)
+
+    print 'mse=',mse
+    print 'rmse=',rmse
+    print 'R2_train=', LRmodel.score(x_train,y_train)     # R^2=1-(RSS残差平方和)/(TSS总平方差和)
+    print 'R2_test=', LRmodel.score(x_test,y_test)
+
+    mpl.rcParams['font.sans-serif'] = [u'simHei']
+    mpl.rcParams['axes.unicode_minus'] = False
+
+    plt.figure()
+    t=np.arange(len(x_test))
+    plt.plot(t,y_test,'r*',label=u'真实数据')
+    plt.plot(t, y_hat, 'g-', label=u'预测数据')
+    plt.legend(loc='upper right')
+    plt.title(u'线性回归销量预测')
+    plt.grid(b=True)
+    plt.show()
+
+
+
 
 
